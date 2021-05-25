453c3f2fed93b5f820427040738e4fedcfb1cd92
Jon Yeatman <jonlyeatman@gmail.com>
2021-05-25 13:29:25 -0400

    File set up for computer guessing

---
+++ computer-guess-number/main.py
@@ -0,0 +1,18 @@
+import random
+
+def guess(x):
+    random_number = random.randint(1, x)
+    guess = 0
+    while guess != random_number:
+        guess = int(input(f'Please guess a number between 1 and {x}: '))
+        if guess < random_number:
+            print(f'Sorry guess again {guess} is too low')
+        elif guess > random_number:
+            print(f'Sorry guess again {guess} is too hight')
+
+    print(f'Congratulations you guessed the correct number {random_number}')
+
+def compute_guess(x)
+    
+
+guess(10)
