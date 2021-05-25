8b29c8d599d95782cf95a3e12923ad82e703faba
Jon Yeatman <jonlyeatman@gmail.com>
2021-05-25 08:19:08 -0400

    Photo option for future use

--- python clock/main.py
+++ python clock/main.py
@@ -7,13 +7,16 @@ root = Tk()
 root.title("clock")
 
 def time():
-    string = strftime('%H:%M:%S %p')
+    string = strftime('%l:%M:%S %p ')
     label.config(text=string)
     label.after(1000, time)
 
-label = Label(root, font=("", 80), background = "black", foreground = "cyan")
+label = Label(root, font=("Digital-7", 200), background = "black", foreground = "cyan")
 label.pack(anchor='center')
 
 time()
 
 mainloop()
+
+# photo = PhotoImage(file = "path to file")
+# root.iconphoto(False, photo)
